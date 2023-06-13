ad822e63159e8311cc54ee59889c079dab224719
younes <elhakimyyouness4@gmail.com>
2023-06-13 01:45:32 +0100

    Display users in simple listView - create User model class

---
+++ app/src/main/java/com/example/mini_project01/User.java
@@ -0,0 +1,34 @@
+package com.example.mini_project01;
+
+public class User {
+    private  String firstname;
+    private  String lastname;
+    private  String gender;
+    private  String city;
+
+    public User(String firstname, String lastname, String gender, String city) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.city = city;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public  String fullname(){
+        return String.format("%s %s",firstname, lastname.toUpperCase());
+    }
+}
